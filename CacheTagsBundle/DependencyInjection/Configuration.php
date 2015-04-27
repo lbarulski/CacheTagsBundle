b7a28df485036c421c765d6f961608a3e7fbadd2
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2015-04-27 12:14:46 +0200

    configuration fix

--- CacheTagsBundle/DependencyInjection/Configuration.php
+++ CacheTagsBundle/DependencyInjection/Configuration.php
@@ -22,14 +22,17 @@ class Configuration implements ConfigurationInterface
 
 		/** @noinspection PhpUndefinedMethodInspection */
 		$rootNode
+			->addDefaultsIfNotSet()
 			->children()
 				->arrayNode('varnish')
+					->addDefaultsIfNotSet()
 					->children()
 						->scalarNode('host')->defaultValue('127.0.0.1')->end()
 						->integerNode('port')->defaultValue(80)->end()
 						->scalarNode('path')->defaultValue('/')->end()
 						->integerNode('timeout')->defaultValue(1)->end()
 						->arrayNode('header')
+							->addDefaultsIfNotSet()
 							->children()
 								->scalarNode('tags')->defaultValue('X-CACHE-TAGS')->end()
 								->scalarNode('invalidation')->defaultValue('X-CACHE-TAG')->end()
